e5d7370f78b42da1baa44d7ce053dc1e3bb8b7f7
ALI ASM IFTEKHAR <ali.iftekhar343@gmail.com>
2018-05-13 01:22:06 +0600

    DbIntegrated And Course Table Done

---
+++ LmsApp.Server/Migrations/20180512191536_IndexCourseTable.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LmsApp.Server.Migrations
+{
+    public partial class IndexCourseTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Topic",
+                table: "Courses",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_IsFree",
+                table: "Courses",
+                column: "IsFree");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Name",
+                table: "Courses",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Topic",
+                table: "Courses",
+                column: "Topic");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_IsFree",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_Name",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_Topic",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Topic",
+                table: "Courses",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
