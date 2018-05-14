7703d1c135d472a73e24b586564aa99af18077be
ALI ASM IFTEKHAR <ali.iftekhar343@gmail.com>
2018-05-15 01:11:37 +0600

    Repository

--- ViewModel/CourseViewModel.cs
+++ ViewModel/CourseViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Common.Model;
 using Common.ViewModel;
 using Model;
 
 namespace ViewModel
 {
-    public class CourseViewModel:BaseViewModel
+    public class CourseViewModel:BaseViewModel<Course>
     {
         public CourseViewModel(Course course):base(course)
         {
             Name = course.Name;
             Topic = course.Topic;
             IsFree = course.IsFree;
+        }
+
+        public CourseViewModel(Entity entity) : base(entity)
+        {
         }
 
         public string Name { get; set; }
