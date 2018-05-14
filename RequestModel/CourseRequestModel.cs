94f77625d80a334c7d7d1083354c7ba2e9f9ecb5
ALI ASM IFTEKHAR <ali.iftekhar343@gmail.com>
2018-05-15 00:55:44 +0600

    ComplexCodeAdded

---
+++ RequestModel/CourseRequestModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Common.RequestModel;
+using Common.ViewModel;
+using Model;
+
+namespace RequestModel
+{
+    public class CourseRequestModel:BaseRequestModel<Course>
+    {
+        public CourseRequestModel(string keyword, string orderBy = "Modified", string isAscending = "false") : base(keyword, orderBy, isAscending)
+        {
+        }
+
+        protected override Expression<Func<Course, bool>> GetExpression()
+        {
+            ExpressionObj = x => x.Name.Contains(Keyword) || x.Topic.Contains(Keyword);
+            ExpressionObj =ExpressionObj.And(GenerateBaseEntityExpression());
+            return ExpressionObj;
+        }
+
+        public override Expression<Func<Course, DropDownViewModel>> Dropdown()
+        {
+            return x => new DropDownViewModel() {Id = x.Id, Text = x.Name, Data = x};
+        }
+    }
+}
